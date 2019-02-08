f30afb6494068af951f5b8342f901b5865c6d42f
Jarkko Lehto <jarkko.lehto@civilpoint.fi>
2019-02-08 12:49:47 +0200

    first commit

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    
+    transform: {
+        "^.+\\.tsx?$": "ts-jest",
+    },
+    
+    testRegex: "(/src/tests/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+    testURL: "http://localhost/"
+};
